830e4eb9a7d2e6e5bb7cd6e03f06ce33a7790b92
Виталий Мартынов <vit9liy@gmail.com>
2022-02-08 21:23:18 +0200

    f_commit

---
+++ PythonStart.py
@@ -0,0 +1,8 @@
+print('Hello, World')
+print('Hello, World', 23232, 5698, 8745)
+print('Hello, World', 23232, 5698, 8745, sep='*')
+print('Hello, World', 23232, 5698, 8745, sep='*', end='!')
+x = int(input('enter number '))
+print(x // 100)
+print(x // 10 % 10)
+print(x % 10)
